1a3ccd1e8a08ef352bbd03e6ac624ad95de9d517
Furbi <williamjohngraham2011@gmail.com>
2026-01-03 22:23:59 +1100

    Add files via upload

---
+++ chooseyourownadventure.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+player_name = input('Hello adventurer! What is your name? ')
+player_health = 100
+player_minimum_damage = 5
+player_maximum_damage = 20
+
+direction = input(f'{player_name}, you enter a dark dungeon. Do you go left towards a faint growl, or right towards the flickering lights?')
